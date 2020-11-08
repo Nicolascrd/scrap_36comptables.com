18549dab764b46b92a2419680be5267219181652
Nicolas Couraud <nic.crd@gmail.com>
2020-11-08 11:11:18 +0100

    Toutes les pages du département

---
+++ README.md
@@ -0,0 +1,10 @@
+Ce script python est lancé à l'aide de la commande python3 script.py (ou python script.py) sur un shell
+
+Il permet de collecter des informations sur un site annuaire d'expert comptables. Les informations sont les suivantes : nom, prénom, email, ville
+dans cet ordre et au format csv.
+
+Il ajoute ces informations à un document dans le même répertoire que le script : repertoire.csv, qui est exportable vers les outils de cold-emailing.
+
+Requiremements :
+Python 3
+Firefox (web browser)
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-"""Lancer le programme avec la command python3 script.py en mettant le département en argument
+"""Lancer le programme avec la command python3 script.py en mettant le département slug correspondant dans la variable DEPARTEMENT
    Tous les departements sont disponibles dans le fichier departement.csv dans le même répertoire."""
 
 # os for file management
@@ -44,61 +44,69 @@ def add2csv(liste):
     f.write(liste[2])
     f.write(',')
     f.write(liste[3])
-    f.write('\n')()
+    f.write('\n')
     f.close()
     return None
 
 driver = webdriver.Firefox()
-url = 'http://36comptables.com/annuaire/' + DEPARTEMENT
 
-driver.get(url)
-liste_pages = driver.find_elements_by_class_name('professional-name')
-startpoint = 10 #pour pas prendre les premiers cabinets qui sont pas des bonnes adresses.
-print(len(liste_pages))
-for i in range(startpoint, len(liste_pages)):
-    secondes = time.time()
-    #on enleve la banière de cookie
-    print("tour start")
+for j in range(1,9):
     try:
-        print("try")
-        cookie = driver.find_element_by_class_name("cookies-eu-ok")
-        cookie.click()
-        liste = driver.find_elements_by_class_name('professional-name')#on refait la liste à chaque fois mais on itère toujours
-        elt = liste[i]
-        elt.click()
-        
+        url = 'https://36comptables.com/annuaire/' + DEPARTEMENT + '/' + str(j)
+        driver.get(url)
     except:
-        print("exept n°1")
-        pass
-    try:
-
-        #a partir d'ici, on est dans la page d'un expert comptable.
-        
-        titre = driver.find_element_by_css_selector("h1")
-        titre_text = titre.text
-        titre_liste = titre_text.split(' ')
-        ville = titre_liste[-1]
-        pro_info = driver.find_element_by_class_name("professional")
-        paragraphes = pro_info.find_elements_by_css_selector('p')
-        para = paragraphes[5].text
-        email = get_email(para)[:-1]
-        name = get_name(para)
-        print(name,email,ville)
-        
-    except:
-        print('except n°2')
         pass
     else:
-        print('else')
-        if (len(name) == 2):
-            ligne = [name[1], name[0], email, ville]
-            add2csv(ligne)
-        pass
-        #et jusque ici 
-    driver.back()  
-    print("driver back\n") 
-    print(time.time()-secondes)  
-print('on arrive la ?')      
+        
+        liste_pages = driver.find_elements_by_class_name('professional-name')
+        startpoint = 240 #pour pas prendre les premiers cabinets qui sont pas des bonnes adresses.
+        print(len(liste_pages))
+        for i in range(startpoint, len(liste_pages)):
+            driver.get(url)
+            print('get url', url)
+            secondes = time.time()
+            #on enleve la banière de cookie
+            print("tour start")
+            try:
+                print("try")
+                cookie = driver.find_element_by_class_name("cookies-eu-ok")
+                cookie.click()
+            except:
+                pass
+            try:
+                liste = driver.find_elements_by_class_name('professional-name')#on refait la liste à chaque fois mais on itère toujours
+                elt = liste[i]
+                elt.click()
+                
+            except:
+                pass
+            try:
+
+                #a partir d'ici, on est dans la page d'un expert comptable.
+                
+                titre = driver.find_element_by_css_selector("h1")
+                titre_text = titre.text
+                titre_liste = titre_text.split(' ')
+                ville = titre_liste[-1]
+                pro_info = driver.find_element_by_class_name("professional")
+                paragraphes = pro_info.find_elements_by_css_selector('p')
+                para = paragraphes[5].text
+                email = get_email(para)[:-1]
+                name = get_name(para)
+                print(name,email,ville)
+                
+            except:
+                print('except n°2')
+                pass
+            else:
+                print('else')
+                if (len(name) == 2):
+                    ligne = [name[1], name[0], email, ville]
+                    add2csv(ligne)
+                pass
+                #et jusque ici   
+            print(time.time()-secondes)       
+
 driver.close()
 
 
